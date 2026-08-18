6d278f73680c79d286006cce6329ede728b75a97
Vince Liem <vincepaul.liem@gmail.com>
2026-08-18 22:59:24 +0200

    update main.go with startup signal

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+const address = ":8080"
+
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "ok")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("server started at http://localhost%s\n", address)
+
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println("server stopped", err)
+	}
 }
