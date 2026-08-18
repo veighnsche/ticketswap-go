cb92434b8cd1691953d12eea3d335be6e7df703a
Vince Liem <vincepaul.liem@gmail.com>
2026-08-19 01:26:43 +0200

    with create events post

--- internal/events/handler.go
+++ internal/events/handler.go
@@ -11,13 +11,29 @@ type Handler struct {
 	DB *sql.DB
 }
 
-func (h Handler) RegisterRoutes(mux *http.ServeMux) {
+// -- Register
+func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("POST /events", h.create)
 	mux.HandleFunc("GET /events", h.list)
 	mux.HandleFunc("GET /events/{id}", h.get)
 }
 
+// POST /events
+func (h *Handler) create(w http.ResponseWriter, r *http.Request) {
+	var event Event
+
+	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+		http.Error(w, "request body must be valid JSON", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(event)
+}
+
 // GET /events
-func (h Handler) list(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) list(w http.ResponseWriter, r *http.Request) {
 	events := []Event{
 		{
 			ID:    1,
@@ -31,7 +47,7 @@ func (h Handler) list(w http.ResponseWriter, r *http.Request) {
 }
 
 // GET /events/{id}
-func (h Handler) get(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) get(w http.ResponseWriter, r *http.Request) {
 	idStr := r.PathValue("id")
 
 	id, err := strconv.Atoi(idStr)
