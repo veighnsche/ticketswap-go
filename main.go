c91f44ba4f26aaafed6ace4902ec6e99e17a909d
Vince Liem <vincepaul.liem@gmail.com>
2026-08-18 23:11:49 +0200

    with /events endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,6 +12,25 @@ func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "ok")
 	})
+
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+
+		events := []map[string]any{
+			{
+				"id":    0,
+				"title": "lowlands",
+				"city":  "Biddinghuizen",
+			},
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(events)
+	})
+
+	// Start the server
 
 	fmt.Printf("server started at http://localhost%s\n", address)
 
