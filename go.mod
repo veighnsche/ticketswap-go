9fc76e50e363555e4f65b1e8eaf90eb2a955baa9
Vince Liem <vincepaul.liem@gmail.com>
2026-08-18 22:46:39 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ticketswap-go
+
+go 1.26.5
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
